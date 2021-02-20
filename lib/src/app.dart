59a4744aa9574224174426aedb357e287de69c5a
Salih Gencer <salih.gencer@turkticaret.net>
2021-02-20 22:00:40 +0300

    homescreen setting

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:journal_app/src/screens/home.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        child: Text("dasdasdas"),
-      ),
+      home: HomeScreen(),
     );
   }
 }
